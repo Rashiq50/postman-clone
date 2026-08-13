0e7759f0509c7eb2cc0e1eccecb2aa70a38ce22f
Rashiq Rahman <rahmanrashiq56@gmail.com>
2026-08-13 19:10:38 +0600

    dsf

--- backend/src/sessions/sessions.controller.ts
+++ backend/src/sessions/sessions.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { API_VERSION } from '@postman-clone/contracts';
+import { SessionsService } from './sessions.service';
 
-@Controller('sessions')
-export class SessionsController {}
+@Controller({ path: 'sessions', version: API_VERSION })
+export class SessionsController {
+  constructor(private readonly sessionsService: SessionsService) {}
+
+}
--- backend/src/sessions/sessions.service.ts
+++ backend/src/sessions/sessions.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { SessionEntity } from './entities/session.entity';
 
 @Injectable()
-export class SessionsService {}
+export class SessionsService {
+    constructor(
+        @InjectRepository(SessionEntity)
+        private readonly sessionsRepository: Repository<SessionEntity>,
+    ) { }
+
+    // TODO: Implement the service methods
+}
